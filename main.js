a94228397c89f8fe54b84abde908eb29fcd935d7
pragyajhala121 <73744592+pragyajhala121@users.noreply.github.com>
2021-03-08 19:36:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup() {
+    canvas = creteCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+function take_snapshot() {
+    save("Filter glow.png")
+}
